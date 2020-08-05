865fd18a96c06188129b4529adc1dd88f63fdcbd
diegotorre5 <diegotorre5@github.com>
2020-08-05 21:47:22 +0200

    Insert in Mongo

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -1,10 +1,31 @@
-const MongoClient = require('mongodb').MongoClient;
-
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+ // var obj = new ObjectID();
+ // console.log(obj);
 MongoClient.connect('mongodb://db:27017/TodoApp', (err, client) => {
   if (err){
     return console.log(`Unable to connect to MongoDB server: ${err}`);
   }
   console.log('Connected to MongoDB server');
   const db = client.db('TodoApp');
+  db.collection('Todos').insertOne({
+    text:'Walk the dog',
+    completed: false
+  }, (err, result) =>{
+    if(err){
+      return console.log('Unable to insert Todo');
+    }
+    console.log(JSON.stringify(result.ops, undefined, 2));
+  });
+  // db.collection('Users').insertOne({
+  //   name: 'Diego',
+  //   age: 36,
+  //   location: 'Nozaru'
+  // }, (err, result) => {
+  //     if(err){
+  //       return console.log('Unable to insert User');
+  //     }
+  //     console.log((JSON.stringify(result.ops[0]._id.getTimestamp(), undefined, 2)));
+  // })
   client.close();
 });

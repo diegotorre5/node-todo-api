f7c781f993dc86808519228537f949d313161b2e
diegotorre5 <diegotorre5@github.com>
2020-08-22 12:33:07 +0200

    Adding log to environmental var to show value Mongo db connection string

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,7 @@ var mongoose = require('mongoose');
 const mongodb_uri = process.env.MONGODB_URI || 'mongodb://db:27017/TodoApp';
 mongoose.promise = global.promise;
 mongoose.connect(mongodb_uri, {useNewUrlParser: true});
+console.log(`Database: ${mongodb_uri}`);
 //gPTOTOIXh7qnh9j8
 //node-user
 module.exports = {mongoose};

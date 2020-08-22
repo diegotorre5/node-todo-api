107d0feb417b6d95cd4bdfa7b699eddf42810bdc
diegotorre5 <diegotorre5@github.com>
2020-08-22 15:32:27 +0200

    useUnifiedTopology set true: warning removed - Database log shown with labes

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
 const mongodb_uri = process.env.MONGODB_URI || 'mongodb://db:27017/TodoApp';
 mongoose.promise = global.promise;
-mongoose.connect(mongodb_uri, {useNewUrlParser: true});
-console.log(`Database: ${mongodb_uri}`);
+mongoose.connect(mongodb_uri, {useNewUrlParser: true, useUnifiedTopology: true});
+console.log(`Database: ${(process.env.MONGODB_URI) ? 'mongo-atlas-heroku' : 'local' }`);
 //gPTOTOIXh7qnh9j8
 //node-user
 module.exports = {mongoose};

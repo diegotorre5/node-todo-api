74768286f093b2b802c5012e4ab132f7fb0f5d5e
diegotorre5 <diegotorre5@github.com>
2020-08-22 12:28:01 +0200

    Adding environmental variable process.env.MONGODB_URI

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,15 +1,7 @@
-//const uri = process.env.MONGODB_URI || "mongodb+srv://node-user:gPTOTOIXh7qnh9j8@mongo-cluster.jxlaa.mongodb.net/TodoApp?retryWrites=true&w=majority";
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
-
 var mongoose = require('mongoose');
+const mongodb_uri = process.env.MONGODB_URI || 'mongodb://db:27017/TodoApp';
 mongoose.promise = global.promise;
-//mongoose.connect(process.env.MONGODB_URI || 'mongodb://db:27017/TodoApp', );
-mongoose.connect('mongodb+srv://node-user:gPTOTOIXh7qnh9j8@mongo-cluster.jxlaa.mongodb.net/TodoApp?retryWrites=true&w=majority', {useNewUrlParser: true});
-//mongodb+srv://node-user:<password>@mongo-cluster.jxlaa.mongodb.net/<dbname>?retryWrites=true&w=majority
+mongoose.connect(mongodb_uri, {useNewUrlParser: true});
 //gPTOTOIXh7qnh9j8
 //node-user
 module.exports = {mongoose};

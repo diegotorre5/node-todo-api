b61f1e914a2ba69f7b65b0d00972ea5121fae1aa
diegotorre5 <diegotorre5@github.com>
2020-08-25 17:48:51 +0200

    Port changed to 3000

--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 var env = process.env.NODE_ENV;
 if(env === 'development') {
-  process.env.PORT = 4000;
+  process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://db:27017/TodoApp';
 } else if (env === 'test') {
-  process.env.PORT = 4000;
+  process.env.PORT = 3000;
   process.env.MONGODB_URI = 'mongodb://db:27017/TodoAppTest';
 }
 

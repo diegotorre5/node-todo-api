6b5d8620f0bbd074f33fe5ffe634d9ebe04d998e
diegotorre5 <diegotorre5@github.com>
2020-08-25 14:44:00 +0200

    //added as decorative

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -3,7 +3,7 @@ mongoose.promise = global.promise;
 mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
 var dbToShow = process.env.MONGODB_URI;
 
-console.log(`Database: ${(dbToShow.replace(dbToShow.substring(dbToShow.search('//'),dbToShow.search('@')),'****:*****'))}`);
+console.log(`Database: ${(dbToShow.replace(dbToShow.substring(dbToShow.search('//'),dbToShow.search('@')),'//****:*****'))}`);
 //gPTOTOIXh7qnh9j8
 //node-user
 module.exports = {mongoose};
